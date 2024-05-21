4f72882f10644b20c6dab24b21bf7ab78929b9a6
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-05-21 12:39:11 +0000

    working on menu toggle

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+
 const menuToggle = document.getElementById('menuToggle');
 
 const dropDownMenu = document.getElementById('dropDownMenu')
 
 menuToggle.addEventListener('click', ()=>{
 
-    if(dropDownMenu.style.display === 'none'){
+    if(dropDownMenu.style.display === 'none'  || dropDownMenu.style.display === ''){
 
         dropDownMenu.style.display = 'flex';
     }
         else{
-            dropDownMenu.style.display = 'none'
+            dropDownMenu.style.display = 'none';
         }
 })
